b4892ce01e5ec13b1c733c78936a1a194932cebe
timofey.podolenchuk <timofey.podolenchuk@x5.ru>
2021-02-07 13:19:49 +0300

    work in progress

--- project_first_app/models.py
+++ project_first_app/models.py
@@ -2,37 +2,41 @@ from django.db import models
 
 
 # Create your models here.
-class Owner(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    birthday = models.DateField()
-    vehicles = models.ManyToManyField('Vehicle', through='Ownership')
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    tasks = models.OneToManyField('Task')
+    isTeacher = models.BooleanField(default=False)
 
 
-class Licence(models.Model):
-    TYPES = (
-        ('A', 'Motocycle'),
-        ('B', 'Car'),
-        ('C', 'Truck'),
-        ('D', 'Bus')
-    )
-    licence_id = models.IntegerField()
-    licence_date = models.DateField()
-    type = models.CharField(max_length=1, choices=TYPES)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+class Task(models.Model):
+    professor = models.ForeignKey(User)
+
+    subject = models.CharField(max_length=50)
+    taskContents = models.CharField()
+
+    releaseDate = models.DateField()
+
+    timePeriod = models.IntegerField()
+    grade = models.IntegerField()
+    penalty = models.IntegerField()
 
 
-class Vehicle(models.Model):
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    color = models.CharField(max_length=50)
-    reg_number = models.CharField(max_length=50)
-
-
-class Ownership(models.Model):
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-    buy_date = models.DateField(blank=True, null=True)
-    sell_date = models.DateField(blank=True, null=True)
+# class Ownership(models.Model):
+#     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+#     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+#     buy_date = models.DateField(blank=True, null=True)
+#     sell_date = models.DateField(blank=True, null=True)
+#
+# class Licence(models.Model):
+#     TYPES = (
+#         ('A', 'Motocycle'),
+#         ('B', 'Car'),
+#         ('C', 'Truck'),
+#         ('D', 'Bus')
+#     )
+#     licence_id = models.IntegerField()
+#     licence_date = models.DateField()
+#     type = models.CharField(max_length=1, choices=TYPES)
+#     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
 
 
